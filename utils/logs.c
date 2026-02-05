acd9b6c8b12108840dd824e992ea26f26ad87f21
Mohamed Tawil <twlmed212@gmail.com>
2026-02-05 17:45:40 +0100

    Protect logs with mutex

--- utils/logs.c
+++ utils/logs.c
@@ -6,7 +6,7 @@
 /*   By: mtawil <mtawil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 16:07:42 by mtawil            #+#    #+#             */
-/*   Updated: 2026/02/05 16:10:07 by mtawil           ###   ########.fr       */
+/*   Updated: 2026/02/05 17:45:28 by mtawil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 void print_logs(t_philo *philo, char *log)
 {
- long timenow;
- 
- timenow = get_timestamp(philo->data->start_time);
- printf("%ld %d %s\n", timenow, philo->id, log);
- 
+	long timenow;
+
+	pthread_mutex_lock(&philo->data->print_mutex);
+
+	timenow = get_timestamp(philo->data->start_time);
+	printf("%ld %d %s\n", timenow, philo->id, log);
+	
+	pthread_mutex_unlock(&philo->data->print_mutex);
+
 }
